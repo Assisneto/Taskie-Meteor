75680d728cc0fd1ddafd1fd24cbda835660931f2
Assisneto <netodamasceno12@gmail.com>
2018-09-17 16:04:30 -0300

    Bootstrap

--- client/list/list.js
+++ client/list/list.js
@@ -3,8 +3,10 @@ Template.list.helpers({
   tarefas : ()=>{
         
         return Tarefas.find({});
-          
-    
+  },
+
+  formataData : ()=>{
+    return moment(this.data).format('DD/MM/YYYY HH:MM')
   }
 
 });
